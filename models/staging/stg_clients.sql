6a2aa55ab3f55ed78a1a2a100fffcdcb934d99d6
chamaillard.stephanie@gmail.com <chamaillard.stephanie@gmail.com>
2026-02-13 14:34:47 +0000

    Cleanup

--- models/staging/stg_clients.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  id_client,
-  nom_client,
-  prenom_client,
-  email_client,
-  id_segment
-FROM {{ source('VTC_Paris', 'clients') }}
-WHERE email_client IS NOT NULL
--- models/staging/stg_segments_clients.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-id_segment,
-segment_client
-FROM `VTC_Paris.segments_clients`
