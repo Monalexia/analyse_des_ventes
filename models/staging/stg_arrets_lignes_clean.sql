51af374f979939b447ae0472a9f252e47bbd1579
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-04 13:12:17 +0000

    Mise à jour arret ligne

--- models/staging/stg_arrets_lignes_clean.sql
+++ models/staging/stg_arrets_lignes_clean.sql
@@ -1,7 +1,7 @@
 SELECT
     TRIM(SPLIT(ID, ':')[SAFE_OFFSET(1)]) AS id_ligne,
     TRIM(ARRAY_REVERSE(SPLIT(stop_id, ':'))[SAFE_OFFSET(0)]) AS id_stop,
-    TRIM(stop_name) AS stop_name,
+    TRIM(stop_name) AS nom_stop,
     stop_lon,
     stop_lat,
     TRIM(nom_commune) AS nom_commune,
