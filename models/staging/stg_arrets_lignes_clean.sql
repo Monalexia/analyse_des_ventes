74d03f093c868491cc568ddabe2a9db0722cae16
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-04 11:36:43 +0000

    Mise à jour trim

--- models/staging/stg_arrets_lignes_clean.sql
+++ models/staging/stg_arrets_lignes_clean.sql
@@ -1,9 +1,9 @@
 SELECT
-    SPLIT(ID, ':')[SAFE_OFFSET(1)] AS id_ligne_clean,
-    ARRAY_REVERSE(SPLIT(stop_id, ':'))[SAFE_OFFSET(0)] AS stop_id_clean,
-    stop_name,
+    TRIM(SPLIT(ID, ':')[SAFE_OFFSET(1)]) AS id_ligne,
+    TRIM(ARRAY_REVERSE(SPLIT(stop_id, ':'))[SAFE_OFFSET(0)]) AS id_stop,
+    TRIM(stop_name) AS stop_name,
     stop_lon,
     stop_lat,
-    nom_commune,
+    TRIM(nom_commune) AS nom_commune,
     code_insee
 FROM {{ source('UrbanMove_raw', 'arrets_lignes') }}
