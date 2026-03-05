d0ea91482ba07c93a89af8e1ede9b421c2a112b7
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-05 08:11:47 +0000

    Mise à jour

--- models/faits_meteo.sql
+++ models/faits_meteo.sql
@@ -1,9 +1,10 @@
 SELECT
     date_jour,
+    heure,
     temperature,
     precipitation,
     neige,
     vent,
     meteo_defavorable
 FROM {{ ref('stg_horaires_clean')}}
-GROUP BY date_jour, temperature, precipitation, neige, vent, meteo_defavorable
+GROUP BY date_jour, heure, temperature, precipitation, neige, vent, meteo_defavorable
