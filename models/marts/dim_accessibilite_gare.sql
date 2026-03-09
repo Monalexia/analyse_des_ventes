cce12f5eb79dbccfe3b06d4aa899bca372a2f9ba
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-09 11:47:44 +0000

    Mise à jour

--- models/marts/dim_accessibilite_gare.sql
+++ models/marts/dim_accessibilite_gare.sql
@@ -1,5 +1,10 @@
 SELECT
     id_niveau_accessibilite,
-    nom_niveau_accessibilite
+    CASE
+        WHEN id_niveau_accessibilite = 1 THEN 'non accessible'
+        WHEN id_niveau_accessibilite = 4 THEN 'sur demande'
+        WHEN id_niveau_accessibilite = 3 THEN 'sur réservation'
+        ELSE 'accessible'
+    END AS nom_niveau_accessibilite
 FROM {{ ref('stg_accessibilite_gare_clean')}}
 GROUP BY id_niveau_accessibilite, nom_niveau_accessibilite
