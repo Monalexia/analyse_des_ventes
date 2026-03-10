da60bd750e1fe1b2704bea019c7e49d1fd57edc9
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-10 09:35:54 +0000

    Mise à jour

--- models/marts/dim_titres_transport.sql
+++ models/marts/dim_titres_transport.sql
@@ -8,10 +8,10 @@ SELECT
     ROW_NUMBER() OVER (ORDER BY nom_titre) AS id_titre,
     nom_titre,
     CASE
-        WHEN nom_titre IN ('amethyste', 'contrat solidarité transport', 'tst', 'fgt') THEN 'Tarifs sociaux'
-        WHEN nom_titre IN ('imagine r', 'navigo') THEN 'Usagers réguliers'
-        WHEN nom_titre IN ('forfaits courts', 'navigo jour') THEN 'Usagers occasionnels'
-        ELSE 'Autres titres'
+        WHEN nom_titre IN ('amethyste', 'contrat solidarité transport', 'tst', 'fgt') THEN 'Sociaux'
+        WHEN nom_titre IN ('imagine r', 'navigo') THEN 'Réguliers'
+        WHEN nom_titre IN ('forfaits courts', 'navigo jour') THEN 'Occasionnels'
+        ELSE 'Autres'
     END AS categorie_titre
 
 FROM titres_uniques
