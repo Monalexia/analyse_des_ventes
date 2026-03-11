dc94b2ca070111fad33e96f2416ba5b5b4580859
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-11 08:56:01 +0000

    Mise à jour

--- models/staging/table_previsions_fer.sql
+++ models/staging/table_previsions_fer.sql
@@ -1,5 +1,6 @@
 SELECT
     h.date_jour,
+    h.annee,
     h.mois,
     h.jour_semaine,
     h.id_ligne,
@@ -16,5 +17,5 @@ FROM {{ ref('stg_horaires_clean')}} h
 LEFT JOIN {{ ref('stg_referentiel_lignes_clean')}} l
     ON h.id_ligne = l.id_ligne
 WHERE h.mode_transport IN ('rail', 'metro')
-GROUP BY h.date_jour, h.mois, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
+GROUP BY h.date_jour, h.annee, h.mois, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
     l.accessibilite, l.air_conditionne
--- models/staging/table_previsions_surface.sql
+++ models/staging/table_previsions_surface.sql
@@ -1,14 +1,6 @@
-WITH validations_surface AS (
-    SELECT
-        date_jour,
-        id_group_ligne,
-        SUM(nb_validations) AS nb_validations
-    FROM {{ ref('stg_nb_surface_clean')}}
-    GROUP BY date_jour, id_group_ligne
-)
-
 SELECT
     h.date_jour,
+    h.annee,
     h.mois,
     h.jour_semaine,
     h.id_ligne,
@@ -18,16 +10,12 @@ SELECT
     h.neige,
     h.temperature,
     h.meteo_defavorable,
-    SUM(h.retard_moyen_minutes) AS retard_moyen_minutes,    
-    s.nb_validations,
+    SUM(h.retard_moyen_minutes) AS retard_moyen_minutes,
     l.accessibilite,
     l.air_conditionne
 FROM {{ ref('stg_horaires_clean')}} h
 LEFT JOIN {{ ref('stg_referentiel_lignes_clean')}} l
     ON h.id_ligne = l.id_ligne
-LEFT JOIN validations_surface s
-    ON l.id_group_ligne = s.id_group_ligne
-    AND h.date_jour = s.date_jour
-WHERE h.mode_transport IN ('bus', 'tram') AND s.nb_validations IS NOT NULL
-GROUP BY h.date_jour, h.mois, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
-    s.nb_validations, l.accessibilite, l.air_conditionne
+WHERE h.mode_transport IN ('bus', 'tram')
+GROUP BY h.date_jour, h.annee, h.mois, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
+    l.accessibilite, l.air_conditionne
