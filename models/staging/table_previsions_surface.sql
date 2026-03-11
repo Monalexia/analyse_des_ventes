a48ff2ccb30fb80edac729513dc5b279400dd276
stephanie chamaillard <chamaillard.stephanie+dbt2@gmail.com>
2026-03-11 09:02:48 +0000

    Mise à jour

--- models/staging/table_previsions_surface.sql
+++ models/staging/table_previsions_surface.sql
@@ -2,6 +2,7 @@ SELECT
     h.date_jour,
     h.annee,
     h.mois,
+    h.jour,
     h.jour_semaine,
     h.id_ligne,
     h.mode_transport,
@@ -17,5 +18,5 @@ FROM {{ ref('stg_horaires_clean')}} h
 LEFT JOIN {{ ref('stg_referentiel_lignes_clean')}} l
     ON h.id_ligne = l.id_ligne
 WHERE h.mode_transport IN ('bus', 'tram')
-GROUP BY h.date_jour, h.annee, h.mois, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
+GROUP BY h.date_jour, h.annee, h.mois, h.jour, h.jour_semaine, h.id_ligne, h.mode_transport, h.precipitation, h.vent, h.neige, h.temperature, h.meteo_defavorable,
     l.accessibilite, l.air_conditionne
